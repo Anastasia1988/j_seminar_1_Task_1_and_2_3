c07258778b99476ddd74e7ede018dc833f1b6e35
Dovidovich Anastasia <sidorova88-88@mail.ru>
2023-06-27 22:17:49 +0300

    Task 1 and 2 java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+//        FirstTask firstTask = new FirstTask();
+//        firstTask.nameOutput();
+
+        SecondTask secondTask = new SecondTask();
+        secondTask.goodTime();
+
+    }
+
 }
